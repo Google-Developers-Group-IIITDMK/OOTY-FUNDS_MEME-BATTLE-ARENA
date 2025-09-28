dc0c503c5505b23b263ccba73f7b5113aa660850
armansuri300105 <arshadmansuri572@gmail.com>
2025-09-28 10:39:48 +0530

    feat(integrate security): integrate the middleware with the room and meme routes

--- server/Routes/memeRoutes.js
+++ server/Routes/memeRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { uploadMeme, voteMeme} from '../controllers/memeController.js';
+import { requireAuth } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.post('/upload', uploadMeme);
-router.post('/vote', voteMeme)
+router.post('/upload', requireAuth, uploadMeme);
+router.post('/vote', requireAuth, voteMeme)
 
 export default router;
--- server/Routes/roomRoutes.js
+++ server/Routes/roomRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createRoom, joinRoom } from '../controllers/roomController.js';
+import { requireAuth } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.post('/create', createRoom);
-router.post('/join/:code', joinRoom);
+router.post('/create', requireAuth, createRoom);
+router.post('/join/:code', requireAuth, joinRoom);
 
 export default router;
